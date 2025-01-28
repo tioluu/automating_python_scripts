8c5a4b8e430032984ef17a11f2c25fa2f967aa83
Tiolu <116491567+tioluu@users.noreply.github.com>
2025-01-28 12:07:11 +0100

    declared 's3_client...' outside function

--- s3_operations.py
+++ s3_operations.py
@@ -1,8 +1,8 @@
 import boto3
 import logging
+s3_client = boto3.client('s3', region_name='us-east-1')
 
 def create_s3_bucket(bucket_name):
-    s3_client = boto3.client('s3', region_name='us-east-1')
     
     logging.info(f"Creating S3 bucket: {bucket_name}")
     s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={
@@ -16,4 +16,3 @@ def upload_file_to_s3(bucket_name, file_name, object_name=None):
     object_name = object_name or file_name
     s3_client.upload_file(file_name, bucket_name, object_name)
     logging.info(f"{file_name} uploaded to {bucket_name} as {object_name}.")
-    
